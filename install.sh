be952f49c08e10328d838e833ef245781b3caf32
SergioGMR <sergiogmr@icloud.com>
2024-10-11 17:14:17 +0100

    workflow

--- install.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-export PLAYWRIGHT_CLI_VERSION=$(bundle exec ruby -e 'puts Playwright::COMPATIBLE_PLAYWRIGHT_VERSION.strip')
-bun install playwright@$PLAYWRIGHT_CLI_VERSION || bun install playwright@next
-./node_modules/.bin/playwright install
-bun run index.js
