fdd89a0fc722c35db3ef7891c7b1e7f22669ef43
Sergio Morales <sergiogmr@icloud.com>
2024-10-13 17:36:10 +0100

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -8,6 +8,10 @@ export default async function handler(req, res) {
 
         // Leer el contenido del archivo
         const data = await fs.readFile(filePath, 'utf8');
+        // Configurar encabezados CORS
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
         // Enviar el contenido del archivo como respuesta JSON
         res.status(200).json(JSON.parse(data));
